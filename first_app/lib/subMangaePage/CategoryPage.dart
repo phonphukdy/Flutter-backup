168946e340c4e8c39c68fa05f833bb5e083d93a4
phonphukdy <phonphukdymono@gmail.com>
2025-05-06 10:46:31 +0700

    backup 6/5/25

--- first_app/lib/subMangaePage/CategoryPage.dart
+++ first_app/lib/subMangaePage/CategoryPage.dart
@@ -12,6 +12,7 @@ class CategoryPage extends StatefulWidget {
 }
 
 class _CategoryPageState extends State<CategoryPage> {
+  TextEditingController txtdate = TextEditingController();
   PickedFile? imgFile;
   final ImagePicker impk = ImagePicker();
 
@@ -106,76 +107,131 @@ class _CategoryPageState extends State<CategoryPage> {
     );
   }
 
+  Future<void> _selectDate() async {
+    try {
+      final DateTime? date = await showDatePicker(
+        context: context,
+        initialDate: DateTime.now(),
+        firstDate: DateTime(1950),
+        lastDate: DateTime(2035),
+      );
+
+      if (date != null) {
+        setState(() {
+          txtdate.text = "${date.day}/${date.month}/${date.year}";
+        });
+      }
+    } catch (err) {
+      print(err);
+    }
+  } // ລໍຖ້າການເຮັດວຽກຂອງເຮົາຢູ່ ຈະບໍ່ຕັດຖິ້ມ
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('ຈັດການຂໍ້ມູນປະເພດສິນຄ້າ')),
       body: Center(
-        child: Stack(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            CircleAvatar(
-              radius: 120.0,
-              backgroundImage:
-                  imgFile == null
-                      ? AssetImage("images/logo.png")
-                      : FileImage(File(imgFile!.path)),
+            Stack(
+              children: [
+                CircleAvatar(
+                  radius: 120.0,
+                  backgroundImage:
+                      imgFile == null
+                          ? AssetImage("images/logo.png")
+                          : FileImage(File(imgFile!.path)),
+                ),
+                Positioned(
+                  bottom: 18.0,
+                  right: 18.0,
+                  child: PopupMenuButton<String>(
+                    itemBuilder:
+                        (c) => [
+                          PopupMenuItem(
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.camera,
+                                color: Colors.green.shade800,
+                                size: 25,
+                              ),
+                              title: Text(
+                                "Camera",
+                                style: TextStyle(
+                                  color: Colors.green.shade800,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              onTap: () {
+                                takePhoto(ImageSource.camera);
+                                Navigator.of(
+                                  context,
+                                ).pop(); // or use Navigator.of(context).pop(); if available
+                              },
+                            ),
+                          ),
+                          PopupMenuDivider(),
+                          PopupMenuItem(
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.image,
+                                color: Colors.green.shade800,
+                                size: 25,
+                              ),
+                              title: Text(
+                                "Gallery",
+                                style: TextStyle(
+                                  color: Colors.green.shade800,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              onTap: () {
+                                takePhoto(ImageSource.gallery);
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ),
+                        ],
+                    icon: Icon(
+                      Icons.camera_alt,
+                      size: 45,
+                      color: Colors.amber.shade900,
+                    ),
+                  ),
+                ),
+              ],
             ),
-            Positioned(
-              bottom: 18.0,
-              right: 18.0,
-              child: PopupMenuButton<String>(
-                itemBuilder:
-                    (c) => [
-                      PopupMenuItem(
-                        child: ListTile(
-                          leading: Icon(
-                            Icons.camera,
-                            color: Colors.green.shade800,
-                            size: 25,
-                          ),
-                          title: Text(
-                            "Camera",
-                            style: TextStyle(
-                              color: Colors.green.shade800,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onTap: () {
-                            takePhoto(ImageSource.camera);
-                            Navigator.of(
-                              context,
-                            ).pop(); // or use Navigator.of(context).pop(); if available
-                          },
-                        ),
-                      ),
-                      PopupMenuDivider(),
-                      PopupMenuItem(
-                        child: ListTile(
-                          leading: Icon(
-                            Icons.image,
-                            color: Colors.green.shade800,
-                            size: 25,
-                          ),
-                          title: Text(
-                            "Gallery",
-                            style: TextStyle(
-                              color: Colors.green.shade800,
-                              fontSize: 20,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onTap: () {
-                            takePhoto(ImageSource.gallery);
-                            Navigator.of(context).pop();
-                          },
-                        ),
-                      ),
-                    ],
-                icon: Icon(
-                  Icons.camera_alt,
-                  size: 45,
-                  color: Colors.amber.shade900,
+            SizedBox(height: 20),
+
+            SizedBox(
+              width:
+                  MediaQuery.of(context).size.width *
+                  0.7, // ຫຍໍ້ຂະໜາດຂອງ SizeBox
+              child: TextField(
+                controller: txtdate,
+                onTap: () {
+                  FocusScope.of(context).requestFocus(FocusNode());
+                  _selectDate();
+                },
+                decoration: InputDecoration(
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide.none, // ບໍ່ເອົາເສັ້ນຂອບ
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.blue.shade500,
+                    ), // ຕອນຄິກເລືອກມັນເສັ້ນຈະສະແດງອອກມາ
+                  ),
+                  filled: true, // ຈະມີສີພື້ນຫຼັງ ຄ້າຍໆ lint
+                  prefix: Icon(
+                    Icons.calendar_today,
+                    size: 25,
+                    color: Colors.green.shade500,
+                  ),
+                  labelText: "ວ.ດ.ປ",
                 ),
               ),
             ),
